156942909c4adefa9748d8bde2d0669e0e2b9587
kimcccheung <38657020+kimcccheung@users.noreply.github.com>
2019-03-01 16:10:07 +0800

    Updated function.js at 2019-3-1
    Updated function.js at 2019-3-1

--- js/functions.js
+++ js/functions.js
@@ -34,6 +34,15 @@ var numbers = [0, 1, 2, 5, 10];
 var cube = map(f,numbers);
 console.log(cube);
 
+//Custom Function Call Map Function
+function multiply(multiplier, ...theArgs) {
+  return theArgs.map(x => multiplier * x);
+}
+
+var arr = multiply(2, 1, 2, 3);
+console.log(arr); // [2, 4, 6]
+
+
 //Function Scope
 // The following variables are defined in the global scope
 var num1 = 20,
@@ -100,9 +109,126 @@ function outside(x) {
 fn_inside = outside(3); // Think of it like: give me a function that adds 3 to whatever you 
 console.log("outside(3): "+fn_inside);
                         // 
-result = fn_inside(5); // returns 
+result = fn_inside(5); // returns 8
 console.log("fn_inside(5): "+result);
 
 
 result1 = outside(3)(5); // returns 8
 console.log("outside(3)(5): "+result1);
+
+
+//Multiply-nested functions
+//scope chaining
+function A(x) {
+  function B(y) {
+    function C(z) {
+      console.log(x + y + z);
+    }
+    C(3);
+  }
+  B(2);
+}
+A(1); // logs 6 (1 + 2 + 3)
+
+
+//Closures - provides a sort of encapsulation for the variables of the inner function.
+
+var pet = function(name) {   // The outer function defines a variable called "name"
+  var getName = function() {
+    return name;             // The inner function has access to the "name" variable of the outer 
+                             //function
+  }
+  return getName;            // Return the inner function, thereby exposing it to outer scopes
+}
+myPet = pet('Vivie');
+   
+console.log(myPet());                     // Returns "Vivie"
+
+//Closures#2
+
+var createPet = function(name) {
+  var sex; //Not accessible by
+  
+  return {
+    setName: function(newName) {
+      name = newName;
+    },
+    
+    getName: function() {
+      return name;
+    },
+    
+    getSex: function() {
+      return sex;
+    },
+    
+    setSex: function(newSex) {
+      if(typeof newSex === 'string' && (newSex.toLowerCase() === 'male' || 
+        newSex.toLowerCase() === 'female')) {
+        sex = newSex;
+      }
+    }
+  }
+}
+
+var pet = createPet('Vivie');
+console.log(pet.getName());                  // Vivie
+
+pet.setName('Oliver');
+pet.setSex('male');
+console.log(pet.getSex());                   // male
+console.log(pet.getName());                  // Oliver
+
+
+//Using the arguments object : The arguments variable is "array-like", but not an array. It is array-like in that it has a numbered index and a length property
+function myConcat(separator) {
+   var result = ''; // initialize list
+   var i;
+   // iterate through arguments
+   for (i = 1; i < arguments.length; i++) {
+      result += arguments[i] + separator;
+   }
+   return console.log(result);
+}
+
+// returns "red, orange, blue, "
+myConcat(', ', 'red', 'orange', 'blue');
+
+// returns "elephant; giraffe; lion; cheetah; "
+myConcat('; ', 'elephant', 'giraffe', 'lion', 'cheetah');
+
+// returns "sage. basil. oregano. pepper. parsley. "
+myConcat('. ', 'sage', 'basil', 'oregano', 'pepper', 'parsley');
+
+//Passing Parameters
+myConcat(', ', 'red', 'orange', 'blue');
+function multiply(a, b) {
+  b = typeof b !== 'undefined' ?  b : 1;
+
+  return a * b;
+}
+
+console.log(multiply(5)); // 5
+
+function multiply2(a, b = 1) {
+  return a * b;
+}
+
+console.log(multiply2(5)); // 5
+
+
+//Arrow Functions: Shorter Functions
+var a = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+];
+
+var a2 = a.map(function(s) { return s.length; });
+
+console.log(a2); // logs [8, 6, 7, 9]
+
+var a3 = a.map(s => s.length);
+
+console.log(a3); // logs [8, 6, 7, 9]

d77bfe1b4295b754592358e5ec918e4326e433e3
kimcccheung <38657020+kimcccheung@users.noreply.github.com>
2019-04-03 17:30:05 +0800

    detailsOfObjectModel.js
    detailsOfObjectModel.js

--- detailsOfObjectModel.js
+++ detailsOfObjectModel.js
@@ -2,7 +2,9 @@
 function Employee(name, dept) {
   this.name = name || '';
   this.dept = dept || 'general';
+  this.id = idCounter++;
 }
+Employee.prototype.specialty = 'none';
 
 function Manager() {
   Employee.call(this);
@@ -12,13 +14,16 @@ function Manager() {
 Manager.prototype = Object.create(Employee.prototype);
 Manager.prototype.constructor = Manager;
 
-function WorkerBee(projs) {
-  Employee.call(this);
+function WorkerBee(name , dept , projs) {
+  //Employee.call(this);
+  this.base = Employee;
+  this.base(name,dept);
   this.projects = projs || [];
 }
-WorkerBee.prototype = Object.create(Employee.prototype);
+//WorkerBee.prototype = Object.create(Employee.prototype);
+WorkerBee.prototype = new Employee;
 //WorkerBee.prototype = new Employee;
-WorkerBee.prototype.constructor = WorkerBee;
+//WorkerBee.prototype.constructor = WorkerBee;
 
 function SalesPerson() {
    WorkerBee.call(this);
@@ -28,14 +33,17 @@ function SalesPerson() {
 SalesPerson.prototype = Object.create(WorkerBee.prototype);
 SalesPerson.prototype.constructor = SalesPerson;
 
-function Engineer(mach) {
-   WorkerBee.call(this);
-   this.dept = 'engineering';
+function Engineer(name, projs, mach) {
+  // WorkerBee.call(this);
+  this.base  = WorkerBee;
+  this.base(name,"engineering",projs);
+   //this.dept = 'engineering';
    this.machine = mach || '';
 }
-Engineer.prototype = Object.create(WorkerBee.prototype)
-//Engineer.prototype = new WorkerBee;
-Engineer.prototype.constructor = Engineer;
+//Engineer.prototype = Object.create(WorkerBee.prototype)
+
+Engineer.prototype = new WorkerBee;
+//Engineer.prototype.constructor = Engineer;
 
 //New Class, derived from WorkerBee
 //The constructor calls the base method, passing as its arguments two of the arguments passed to the constructor ("Doe, Jane" and ["navigator", "javascript"]) and also the string "engineering". Explicitly using "engineering" in the constructor indicates that all Engineer objects have the same value for the inherited dept property, and this value overrides the value inherited from Employee.
@@ -47,7 +55,7 @@ function Engineer2(name, projs, mach) {
   this.machine = mach || '';
 }
 
-
+Engineer2.prototype = new WorkerBee;
 //Creating objects with simple definitions
 
 var jim = new Employee; 
@@ -79,18 +87,40 @@ var fred = new SalesPerson;
 // fred.quota is 100
 
 var jane = new Engineer('belau');
-jane.name == '';
-jane.dept == 'engineering';
-jane.projects == [];
-jane.machine == 'belau';
-
-console.log("Jane Properties: " + jane.machine);
-
+jane.name = '';
+jane.dept = 'engineering';
+jane.projects = [];
+jane.machine = 'belau';
+Engineer.prototype.specialty = 'none2';
+console.log("Jane machine: " + jane.machine);
+console.log("Jane specialty: " + jane.specialty);
 
 //add that property to all objects that inherit properties from the prototype. 
-Employee.prototype.specialty = 'none';
+Employee.prototype.specialty = 'none3';
 
-console.log("mark specialty property: "+Employee.prototype.specialty );
+//Employee2.prototype.specialty = 'none';
+console.log("mark(WorkerBee) specialty property: "+mark.specialty );
 
-var jane2 = new Engineer2('Doe, Jane', ['navigator', 'javascript'], 'belau');
+var jane = new Engineer('Doe, Jane', ['navigator', 'javascript'], 'belau');
 
+Engineer2.prototype.specialty = 'none';
+
+console.log("Jane specialty property: "+jane.specialty );
+
+var isTrue = (jane instanceof Engineer2);
+console.log('jane instanceof Employee2?' + isTrue);
+
+
+
+// function instanceOf(object, constructor) {
+//    object = object.__proto__;
+//    while (object != null) {
+//       if (object == constructor.prototype)
+//          return true;
+//       if (typeof object == 'xml') {
+//         return constructor.prototype == XML.prototype;
+//       }
+//       object = object.__proto__;
+//    }
+//    return false;
+// }
